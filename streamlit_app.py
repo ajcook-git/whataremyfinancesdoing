437fa765331c509aeb28216fbd947c59183bfb7f
Adam <59202294+ajcook-git@users.noreply.github.com>
2024-05-31 20:27:00 +0100

    new

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,6 @@ import streamlit as st
 This is a dashboard for Adam. Yes, I know it's public.
 """
 
-df = pd.read_csv('data/newdata.csv', index='Date')
+df = pd.read_csv('data/newdata.csv', index_col='Date')
 
 st.dataframe(df)
