b663d54d66b9f9e7be89b0fde905e86c938e7143
Adam <59202294+ajcook-git@users.noreply.github.com>
2024-05-31 20:22:22 +0100

    new

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,18 +4,10 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+# Adam's finances
+This is a dashboard for Adam. Yes, I know it's public.
 """
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
-
-df = pd.read_csv('data/newdata.csv')
+df = pd.read_csv('data/newdata.csv', index='Date')
 
 st.dataframe(df)
