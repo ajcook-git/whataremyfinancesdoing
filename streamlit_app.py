a9a68e536059fc5e13a59a146f4cb2cf67f99b2a
Adam <59202294+ajcook-git@users.noreply.github.com>
2024-05-31 21:35:05 +0100

    new

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,38 @@
-import altair as alt
-import numpy as np
+# import altair as alt
+# import numpy as np
+import datetime
 import pandas as pd
+import plotly.express as px
 import streamlit as st
+import textwrap
 
 """
 # Adam's finances
 This is a dashboard for Adam. Yes, I know it's public.
-
-I feel like this is a really cool thing - Streamlit.
 """
 
 df = pd.read_csv('data/newdata.csv', index_col='Date')
 
-credit_cols = {'Barclaycard', 'NatwestCredit'}
-debit_cols = list(set(df.columns).difference(credit_cols))
+if all(df.iloc[datetime.datetime.now().month].notna()):
+    textwrap.dedent(st.write("""
+        *Warning:* it looks like your data is out of date!
+    """)
 
-st.write("## These lines represent actual cash")
-st.line_chart(df[debit_cols])
+credit_accs = {'Barclaycard', 'NatwestCredit'}
+credit_cols = list(credit_accs)
+debit_accs = set(df.columns).difference(credit_cols)
+debit_cols = list(debit_accs)
 
-st.write("## These lines represent credit card debt")
-st.line_chart(df[list(credit_cols)])
+debit_df = df[debit_cols]
+debit_df['Total'] = debit_df.transpose().sum()
+credit_df = df[credit_cols]
+credit_df['Total'] = credit_df.transpose().sum()
 
-st.write("## Here's the raw data I'm reading from")
+st.write("### These lines represent actual cash")
+st.line_chart(debit_df)
+
+st.write("### These lines represent credit card debt")
+st.line_chart(credit_df)
+
+st.write("### Here's the raw data I'm reading from")
 st.dataframe(df)
