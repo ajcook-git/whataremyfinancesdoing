4c75e320808b50e78e0f5a12a42ca453d83159c0
Adam <59202294+ajcook-git@users.noreply.github.com>
2024-05-31 20:56:59 +0100

    new

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,8 @@ import streamlit as st
 """
 # Adam's finances
 This is a dashboard for Adam. Yes, I know it's public.
+
+I feel like this is a really cool thing - Streamlit.
 """
 
 df = pd.read_csv('data/newdata.csv', index_col='Date')
@@ -13,6 +15,11 @@ df = pd.read_csv('data/newdata.csv', index_col='Date')
 credit_cols = {'Barclaycard', 'NatwestCredit'}
 debit_cols = list(set(df.columns).difference(credit_cols))
 
+st.write("## These lines represent actual cash")
 st.line_chart(df[debit_cols])
 
+st.write("## These lines represent credit card debt")
+st.line_chart(df[debit_cols])
+
+st.write("## Here's the raw data I'm reading from")
 st.dataframe(df)
