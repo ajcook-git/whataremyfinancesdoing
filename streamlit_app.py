954e0b6f254788b3a99079595e7ead27d595b6c9
Adam <59202294+ajcook-git@users.noreply.github.com>
2024-06-01 15:07:46 +0100

    hope

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ net_now = debit_now - credit_now
 net_last = debit_last - credit_last
 
 if net_last >= 0:
-    home_message = "Adam, your finances are looking :geen[good]!"
+    home_message = "Adam, your finances are looking :green[good]!"
 else:
     home_message = "Adam, your finances went :red[down] this month; take a look."
 
