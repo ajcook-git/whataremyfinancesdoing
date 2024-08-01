b53fd06d3f122d85781f1fa235df0b20dd5553db
Adam <mail.ajcook@gmail.com>
2024-08-01 20:13:41 +0100

    Created separate file for data wrangling

---
+++ data_prep.py
@@ -0,0 +1,38 @@
+""" Provides functions to load various data """
+
+import pandas as pd
+
+# TODO: Replace with a connectors to Google Sheets
+
+CURRENT_ACCOUNTS = ['MoneyboxCashISA', 'MoneyboxS&SISA', 'NatwestSavings', 'UlsterBank']
+CURRENT_ACCOUNTS_DESC = ['Cash ISA', 'Stocks/Shares ISA', 'Regular Saver', 'Easy-access Saver']
+CREDIT_ACCOUNTS = ['Barclaycard', 'NatwestCredit']
+CREDIT_ACCOUNTS_DESC = ['Barclays Credit', 'Natwest Credit']
+
+def load_account_data(exclude: list[str] = None) -> pd.DataFrame:
+    """ Load and pre-process accounts data """
+    # loads bank balances
+    df = pd.read_csv('data/newdata.csv', index_col='Date')
+    df.rename(dict(zip(CURRENT_ACCOUNTS + CREDIT_ACCOUNTS,
+                       CURRENT_ACCOUNTS_DESC + CREDIT_ACCOUNTS_DESC)),
+              axis=1, inplace=True)
+
+    try:
+        df.drop(exclude, axis=1, inplace=True)
+    except KeyError:
+        raise ValueError("Account name(s) from exclude list not found in: "
+                        f"{', '.join(df.columns)}") from None
+
+    # Add totals
+    df['AccTotals'] = df[CURRENT_ACCOUNTS_DESC].transpose().sum()
+    df['CredTotals'] = df[CREDIT_ACCOUNTS_DESC].transpose().sum()
+    df['NetTotals'] = df['AccTotals'] - df['CredTotals']
+
+    return df
+
+def load_creditscores() -> pd.DataFrame:
+    """ Load a pre-process credit score data """
+    df = pd.read_csv('data/creditscores.csv', index_col='Date',
+                    parse_dates=True)
+
+    return df
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,183 +1,108 @@
+"""
+This application is for showing my current finances, in pretty graphs.
+
+"""
+
 import datetime
-import pandas as pd
+import textwrap
 import plotly.express as px
 import streamlit as st
-import textwrap
+import data_prep
 
-def data_load():
-    # loads bank balances
-    x = pd.read_csv('data/newdata.csv', index_col='Date')
-    x.rename({
-         'MoneyboxCashISA': 'Cash ISA',
-         'MoneyboxS&SISA': 'Stocks/Shares ISA',
-         'NatwestSavings': 'Regular Saver',
-         'UlsterBank': 'Easy-access Saver',
-         'Barclaycard': 'Credit card (primary)',
-         'NatwestCredit': 'Credit card (secondary)'
-    }, axis=1, inplace=True)
+st.set_page_config(page_title="Finances", page_icon="💵")
 
-    # loads credit scores
-    y = pd.read_csv('data/creditscores.csv', index_col='Date',
-                    parse_dates=True)
+# load data
+df = data_prep.load_account_data(exclude=['Marcus'])
+credit_df = data_prep.load_creditscores()
 
-    return x, y
+def _calc_date_indices() -> tuple[str]:
+    """ Get values for the current and previous month """
+    today = datetime.date.today()
+    last_month = today.replace(day=1) - datetime.timedelta(days=1)
 
-# Un-comment for wide screen
-# st.set_page_config(layout="wide")  
+    return today.strftime('%Y-%m'), last_month.strftime('%Y-%m')
 
-df, csdf = data_load()
+month_now, month_previous = _calc_date_indices()
+ACC_NOW = df.loc[month_now, 'AccTotals']
+ACC_PREVIOUS = df.loc[month_previous, 'AccTotals']
+CREDIT_NOW = df.loc[month_now, 'CredTotals']
+CREDIT_PREVIOUS = df.loc[month_previous, 'CredTotals']
 
-# Separate credit and debit accounts
-credit_accs = {'Credit card (primary)', 'Credit card (secondary)'}
-credit_cols = list(credit_accs)
-debit_accs = set(df.columns).difference(credit_cols)
-debit_cols = list(debit_accs)
-debit_cols.remove("Marcus")
+NET_NOW = df.loc[month_now, 'NetTotals']
+NET_PREVIOUS = df.loc[month_previous, 'NetTotals']
 
-debit_df = df[debit_cols]
-credit_df = df[credit_cols]
+def create_totals():
+    """ Create totals container """
 
-# Calculate totals and differences from previous month
-month_now = datetime.datetime.now().month -1
-month_last = month_now - 1
-debit_now = debit_df.iloc[month_now].sum()
-debit_last = debit_df.iloc[month_last].sum()
-credit_now = credit_df.iloc[month_now].sum()
-credit_last = credit_df.iloc[month_last].sum()
-net_now = debit_now - credit_now
-net_last = debit_last - credit_last
+    with st.container(border=True):
+        st.write(f"Taking into account data up to {datetime.datetime.strptime(month_now, '%Y-%m').strftime('%B')}")
+        col1, col2, col3 = st.columns(3)
+        col1.metric("Total in Accounts", f"£{ACC_NOW:,.2f}", f"{ACC_NOW-ACC_PREVIOUS:,.2f}")
+        col2.metric("Total in Credit", f"£{CREDIT_NOW:,.2f}", f"{CREDIT_NOW-CREDIT_PREVIOUS:,.2f}",
+                    delta_color='inverse')
+        col3.metric("Total Net", f"£{NET_NOW:,.2f}", f"{NET_NOW-NET_PREVIOUS:,.2f}")
 
-if net_now - net_last >= 0:
-    home_message = "Adam, your finances are looking :green[good]! 💸"
-else:
-    home_message = "Adam, your finances went :red[down] this month."
-    if net_now >= 0:
-        home_message += " But overall, you're still :green[positive]! 😀"
-
-multi = f"""
-# Financial Health
+st.markdown(f"""
+# Finances
 **{datetime.datetime.now().strftime('%A %d %B, %Y')}**
+""")
 
-{home_message}
-"""
-st.markdown(multi)
-
-if not all(df.iloc[month_now].notna()):
+if not all(df.loc[month_now].notna()):
     st.write(textwrap.dedent("""
         **Warning:** it looks like your data is out of date!
     """))
 
-# Quick summmary at the top of the page
-st.write("### Summary")
-with st.container():
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        st.metric(
-            "Debit",
-            f"£{debit_now:,.2f}",
-            f"{debit_now-debit_last:,.2f}"
-        )
+create_totals()
 
-    with col2:
-        st.metric(
-            "Credit",
-            f"£{credit_now:,.2f}",
-            f"{credit_now-credit_last:,.2f}",
-            delta_color='inverse'
-        )
+summary, acc_tab, credit_tab, creditscore_tab, todo_tab = st.tabs(
+    ['Summary', 'Accounts', 'Credit', 'Credit Scores', 'To do'])
 
-    with col3:
-        st.metric(
-            "Net worth",
-            f"£{net_now:,.2f}",
-            f"{net_now-net_last:,.2f}"
-        )
+with summary:
+    if NET_NOW - NET_PREVIOUS >= 0:
+        HOME_MESSAGE = "Your finances are looking :green[good]! 💸"
+    else:
+        HOME_MESSAGE = "Your finances went :red[down] this month."
+        if NET_NOW >= 0:
+            HOME_MESSAGE += " But overall, you're still :green[positive]! 😀"
+    st.write(f"### Summary\n{HOME_MESSAGE}")
+    totals_fig = px.line(df[['AccTotals', 'CredTotals', 'NetTotals']],
+                        labels=dict(value='Amount (£)', Date=""),
+                        title="Total in Accounts, and on Credit Card",
+                        color_discrete_sequence=['lightgreen', 'tomato', 'darkslategray'])
+    totals_fig.update_xaxes(dtick="M1", tickformat="%b\n%Y", ticklabelmode="period")
+    st.plotly_chart(totals_fig)
 
-view_tab, edit_tab, creditscore_tab, todo_tab = st.tabs(['View', 'Edit', 'Credit Scores', 'To do'])
-with view_tab:
-    st.write("""
-    ### These lines represent accounts with actual money in
-    You'll notice I've only selected my "main" accounts (missing: Marcus)
-    """)
-    debit_fig = px.line(
-        debit_df,
-        labels=dict(
-            value='Amount (£)',
-            Date=""
-        ),
-        title="Current value of accounts over time"
-    )
-    debit_fig.update_xaxes(
-        dtick="M1",
-        tickformat="%b\n%Y",
-        ticklabelmode="period"
-    )
-    debit_fig.update_layout(
-        legend=dict(
-           orientation='h',
-           y=-0.3,
-           yanchor='auto',
-        ),
-        legend_title=dict(
-            text=None
-        )
-    )
+with acc_tab:
+    st.write("### Account Balance")
+    debit_fig = px.line(df[data_prep.CURRENT_ACCOUNTS_DESC], labels=dict(value='Amount (£)', Date=""),
+        title="Current value of accounts over time")
+    debit_fig.update_xaxes(dtick="M1", tickformat="%b\n%Y", ticklabelmode="period")
+    # debit_fig.update_layout(legend=dict(orientation='h', y=-0.3, yanchor='auto',),
+    #                         legend_title=dict(text=None))
     st.plotly_chart(debit_fig)
 
-    st.divider()
-    
-    st.write("""
-    ### These lines represent credit card debt
-    The "primary" account is Barclaycard, the "secondary" account is Natwest
-    """)
-    credit_fig = px.line(
-        credit_df,
-        labels=dict(
-            value='Amount (£)',
-            Date=""
-        ),
-    )
-    credit_fig.update_xaxes(
-        dtick="M1",
-        tickformat="%b\n%Y",
-        ticklabelmode="period",
-    )
-    credit_fig.update_layout(
-        legend=dict(
-           orientation='h',
-           y=-0.3,
-           yanchor='auto',
-        ),
-        legend_title=dict(
-            text=None
-        )
-    )
-    st.plotly_chart(credit_fig)
-
-with edit_tab:
-    edited_df = st.data_editor(df)
-    edited_df.to_csv('data/newdata.csv')
-
-    col1, col2, _, _, _, _ = st.columns(6)
-    with col1:
-        if st.button('Update'):
-            st.rerun()
-    
-    with col2:
-        st.download_button("Download", open('data/newdata.csv', 'r'), 'data.csv')
+with credit_tab:
+    pass
+    # st.write("### Credit Card Balance")
+    # credit_fig = px.line(credit_df, labels=dict(value='Amount (£)', Date=""))
+    # credit_fig.update_xaxes(dtick="M1", tickformat="%b\n%Y", ticklabelmode="period")
+    # credit_fig.update_layout(legend=dict(orientation='h', y=-0.3, yanchor='auto', ),
+    #                          legend_title=dict(text=None))
+    # st.plotly_chart(credit_fig)
 
 with creditscore_tab:
+    pass
     # I use 3 providers to access all three brokers' scores in the UK:
     # - Experian = Experian (score/999)
     # - Equifax = Clear Score (score/1000)
     # - TransUnion = Credit Karma (score/710)
-    month_now = datetime.datetime.now().strftime('%Y-%m')
-    mask = [999, 1_000, 710]
+    # month_now = datetime.datetime.now().strftime('%Y-%m')
+    # mask = [999, 1_000, 710]
 
-    data = csdf.loc[month_now, 'Equifax'] / 999
+    # # data = csdf.loc[month_now, 'Equifax'] / 999
 
-    fig = px.pie()
-    st.plotly_chart(fig)
+    # fig = px.pie()
+    # st.plotly_chart(fig)
 
 with todo_tab:
     things_to_do = textwrap.dedent("""
@@ -193,6 +118,7 @@ with todo_tab:
         - Create personal CPI for products tailored to me
         - Track emergency fund
         - Track local property value 
+        - Show amount invested vs. cash vs. credit
     """)
     st.write(f"""
     ### Summary of ideas to implement next
